ebf958b1af6554e6c884ebcaafcec75e00119823
Hajdu Csaba <kyberszittya@protonmail.ch>
2026-02-22 12:27:57 +0100

    Adding hash tests

--- hymeko/parser/src/ir/mod.rs
+++ hymeko/parser/src/ir/mod.rs
@@ -1,6 +1,6 @@
 pub mod ir;
 mod meta;
-mod hash;
+pub mod hash;
 pub mod lower;
 mod time;
 pub mod common;
--- hymeko/parser/tests/intermediate_tests/mod.rs
+++ hymeko/parser/tests/intermediate_tests/mod.rs
@@ -1,3 +1,4 @@
 mod ir_transformation_tests;
 mod ir_value_tests;
 mod ir_test_hash_pass;
+mod ir_blake3_hash_tests;
